03495a7d1c32806df5505a7aec2992241b7f7ea5
Ian Stuart <ian.stuart@ed.ac.uk>
2018-10-09 10:15:30 +0100

    debugging course_code

--- nbgrader/noteable.py
+++ nbgrader/noteable.py
@@ -15,7 +15,9 @@ def get_coursecode():
     This has come via the LTI connection
     """
     print("returning course code", os.environ.get('NAAS_COURSE_CODE', None))
-    return os.environ.get('NAAS_COURSE_CODE', None)
+    course_code = os.environ.get('NAAS_COURSE_CODE', None)
+    print("######## course_id:{}".format(couse_code))
+    return course_code
 
 def self_owned(path):
     """Is the path owned by the current user of this process?"""
